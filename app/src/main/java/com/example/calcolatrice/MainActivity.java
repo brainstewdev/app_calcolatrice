1e671df68d8fae4e32f6ade9f2bfd87841b2b130
brainstew927 <brainstew927@gmail.com>
2019-03-08 23:11:53 +0100

    modificato ancora

--- app/src/main/java/com/example/calcolatrice/MainActivity.java
+++ app/src/main/java/com/example/calcolatrice/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.calcolatrice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +15,31 @@ public class MainActivity extends AppCompatActivity {
 
         String result = "";
 
+        final EditText text = (EditText) findViewById(R.id.editText);
+
+        Button but_1 =  (Button) findViewById(R.id.but_1);
+        but_1.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            public void onClick(View v) {
+                text.setText(text.getText()+"1");
+            }
+        });
+
+        Button but_2 =  (Button) findViewById(R.id.but_2);
+        Button but_3 =  (Button) findViewById(R.id.but_3);
+        Button but_4 =  (Button) findViewById(R.id.but_4);
+        Button but_5 =  (Button) findViewById(R.id.but_5);
+        Button but_6 =  (Button) findViewById(R.id.but_6);
+        Button but_7 =  (Button) findViewById(R.id.but_7);
+        Button but_8 =  (Button) findViewById(R.id.but_8);
+        Button but_9 =  (Button) findViewById(R.id.but_9);
+        Button but_0 =  (Button) findViewById(R.id.but_0);
+
+        Button but_p =  (Button) findViewById(R.id.but_p);
+        Button but_eq =  (Button) findViewById(R.id.but_eq);
 
 
     }
+
+
 }
